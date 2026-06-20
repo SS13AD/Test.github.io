708444c628ae6cbbd40280e64ab11526fefc362d
SS13AD <dimahodyackov@yandex.ru>
2026-06-20 21:10:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
-tg.MainButton.text='asd';
-tg.MainButton.show();
-//let button = document.querySelector('#ABC');
-//button.onclick=()=>{
-//    tg.MainButton.setText('Button')
-//    tg.MainButton.show()
-//}
+let button = document.querySelector('#ABC');
+button.onclick=()=>{
+    tg.MainButton.setText('Button')
+    tg.MainButton.show()
+}
